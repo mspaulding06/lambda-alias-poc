358d995cc9d0be1d2887cf58877217683af7dd1c
Matt Spaulding <matt.spaulding@nwea.org>
2017-11-09 08:54:51 -0800

    Add dedicated role for POC

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_function" "poc" {
   timeout          = 10
   publish          = true
   description      = "Hello World Test"
-  role             = "arn:aws:iam::118000709326:role/full-catLambda-role"
+  role             = "${aws_iam_role.alias_poc_role.arn}"
   source_code_hash = "${base64sha256(file("helloworldapp.zip"))}"
 }
 
@@ -16,4 +16,29 @@ resource "aws_lambda_alias" "poc_alias" {
   description      = "Stable Alias"
   function_name    = "${aws_lambda_function.poc.arn}"
   function_version = "${aws_lambda_function.poc.version}"
+}
+
+resource "aws_iam_role" "alias_poc_role" {
+  name = "alias_poc_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "alias_poc_role_attach" {
+  role       = "${aws_iam_role.alias_poc_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
